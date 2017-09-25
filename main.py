d1ef9a8fa3c6498b84257db5521602d9a2ad0628
Spencer Tyminski <nynjaorca@gmail.com>
2017-09-25 09:33:46 -0500

    Included the use of templates

--- main.py
+++ main.py
@@ -1,63 +1,44 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from caeser import rotate_string
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-    <!DOCTYPE html>
-
-    <html>
-        <head>
-            <style>
-                form {{
-                    background-color: #eee;
-                    padding: 20px;
-                    margin: 0 auto;
-                    width: 540px;
-                    font: 16px sans-serif;
-                    border-radius: 10px;
-                }}
-                textarea {{
-                    margin: 10px;
-                    width: 540px;
-                    height: 120px;
-                }}
-            </style>
-        </head>
-        <body>
-            <!-- create your form here -->
-            <form action="/" method ="post">
-                <label> Rotate by:
-                    <input type="text" name="rot"/>
-                    <textarea name="text">
-                    {0}
-                    </textarea>
-                </label>
-                <input type="submit" value="Submit Query"/>
-            </form>
-        </body>
-    </html>
-    """
 
 @app.route('/', methods=['POST'])
 def encrypt():
+    
+    template = jinja_env.get_template('caeser_form.html')
+    value_error_template = jinja_env.get_template('value_error_template.html')
+
+
+
     rot_value = request.form['rot']
 
     try:
         rot_value = int(rot_value)
     except ValueError:
-        error = "You have entered something that isn't a number."
-        return redirect('/?error=' + error)
+        return value_error_template.render()
 
     user_message = request.form['text']
 
-    encrypted_message = rotate_string(user_message, rot_value)
+    encrypted_message = rotate_string(cgi.escape(user_message), rot_value)
 
-    return form.format("Your most recent encrypted message: " + encrypted_message)
+    return template.render() + "<strong>Your encrypted message:</strong> " + cgi.escape(encrypted_message)
+
+
 
 @app.route("/")
 def index():
-    return (form.format(''))
+    template = jinja_env.get_template('caeser_form.html')
+    return template.render()
+
 
 app.run()
